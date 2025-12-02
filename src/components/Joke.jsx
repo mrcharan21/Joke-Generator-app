d82e5fa5c2bf5e3143f0911d0c4a58a3facce913
charan <89964531+mrcharan21@users.noreply.github.com>
2025-12-02 15:04:31 +0530

    updated

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,3 +1,5 @@
+// Joke.js
+
 import React from "react";
  
 import Button from "./Button";
@@ -5,24 +7,24 @@ import '../assets/joke.css';
 
 const Joke = () => {
     const [Joke, setJoke] = React.useState("");
-    const [loading, setLoading] = React.useState(false);
+    const [loading, setLoading] = React.useState(false); // Add loading state
 
     const fetchApi = () => {
-        setLoading(true);
-        fetch("https://official-joke-api.appspot.com/random_joke")
+        setLoading(true); // Set loading to true before fetching
+        fetch("https://official-joke-api.appspot.com/jokes/programming/random")
             .then((res) => res.json())
             .then((data) => {
-            setJoke(data.setup + " " + data.punchline);
-            setLoading(false);
+            setJoke(data[0].setup + " " + data[0].punchline);
+            setLoading(false); // Set loading to false after fetching
             });
-    }
+    };
 
     return (
         <div className="joke">
             <Button callApi={fetchApi} /> 
-            <p>{loading ? "Loading..." : Joke}</p>    
+            {loading ? <p>Loading...</p> : <p>{Joke}</p>} {/* Show loading text */}
         </div>
     );
-};
+}
 
 export default Joke;
